3c0c5e9af5e21300e90a7436bd7dbb77a73cd71f
obsidion-king <154094983+obsidion-king@users.noreply.github.com>
2025-07-22 00:09:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
 # main.py
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/plot', methods=['POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def plot():
-    data = request.json
-    x = [float(i) for i in data['x'].split(",")]
-    y = [float(i) for i in data['y'].split(",")]
-    axis_x = float(data['axis_x'])
-    axis_y = float(data['axis_y'])
+    if request.method == "POST":
+        try:
+            x = [float(i) for i in request.form["x"].split(",")]
+            y = [float(i) for i in request.form["y"].split(",")]
+            axis_x = float(request.form["axis_x"])
+            axis_y = float(request.form["axis_y"])
 
-    x_output = []
-    y_output = []
 
-    for value in x:
-        orig = value
-        if value < 1:
-            value *= 10
-        x_output.append(f"Plot {round(orig, 3)} at {round(value * axis_x, 3)}CM on the x axis")
+            x_output = []
+            y_output = []
 
-    for value in y:
-        orig = value
-        if value < 1:
-            value *= 10
-        y_output.append(f"Plot {round(orig, 3)} at {round(value * axis_y, 3)}CM on the y axis")
+            for value in x:
+                orig = value
+                if value < 1:
+                    value *= 10
+                x_output.append(
+                    f"Plot {round(orig, 3)} at {round(value * axis_x, 3)}CM on the x axis"
+                )
 
-    return jsonify({'x_axis': x_output, 'y_axis': y_output})
+            for value in y:
+                orig = value
+                if value < 1:
+                    value *= 10
+                y_output.append(
+                    f"Plot {round(orig, 3)} at {round(value * axis_y, 3)}CM on the y axis"
+                )
 
-if __name__ == '__main__':
+            return render_template("index.html", x_output=x_output, y_output=y_output)
+        
+        except (KeyError, ValueError, TypeError) as e:
+            return render_template("index.html", error=str(e))
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
